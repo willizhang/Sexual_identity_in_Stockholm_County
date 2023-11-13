e7ef9a1731143483d49598322cc8c0042a437f86
Willi <guoqiang.w.zhang@gmail.com>
2023-11-13 16:59:58 +0100

    wrote codes to investigate the associations between demographic variables and sexual identites, including crude and adjusted analyses; made forest plots

--- Helper_function.R
+++ Helper_function.R
@@ -5,6 +5,21 @@
 
 ##### Function for Weighted Regression Models #####
 
+### crude analysis ###
+fit_model_crude <- function( formula, design ) {
+  
+  # Poisson regression
+ tryCatch(
+    svyglm( formula, design = design, family = quasipoisson( link = "log" ) ),
+    error = function( e ) {
+      message( e$message )
+      return ( NULL )
+    }
+  )
+}
+
+
+### adjusted analysis ###
 fit_models <- function( formula, design ) {
   
   models <- list()
@@ -42,7 +57,26 @@ fit_models <- function( formula, design ) {
 
 ##### Function to Extract Coefficients and Confidence Intervals (on log scale) #####
 
-extract_results_coef <- function( all_models_list, exposure, outcome, model_type, variables, design ) {
+### crude analysis ###
+extract_results_coef_crude <- function( all_models_list, exposure, outcome, variables ) {
+  
+  model <- all_models_list[[ exposure ]][[ outcome ]]
+  
+  # check if model is available (i.e., converged)
+  if ( !is.null( model ) ) {
+    
+    results <- cbind( coef( model )[ variables ], 
+                      confint( model, ddf = degf( model$survey.design ) )[ variables, , drop = FALSE ] )
+    
+    return( results )
+  } else {
+    return( NULL )
+  }
+}
+
+
+### adjusted analysis ###
+extract_results_coef <- function( all_models_list, exposure, outcome, model_type, variables ) {
   
   model <- all_models_list[[ exposure ]][[ outcome ]][[ model_type ]]
   
@@ -50,7 +84,7 @@ extract_results_coef <- function( all_models_list, exposure, outcome, model_type
   if ( !is.null( model ) ) {
     
     results <- cbind( coef( model )[ variables ], 
-                      confint( model, ddf = degf( design ) )[ variables, , drop = FALSE ] )
+                      confint( model, ddf = degf( model$survey.design ) )[ variables, , drop = FALSE ] )
     
     return( results )
     } else {
@@ -61,6 +95,38 @@ extract_results_coef <- function( all_models_list, exposure, outcome, model_type
 
 ##### Function to Extract Proportion Ratios and Confidence Intervals for Each Model Across Exposures #####
 
+### crude analysis ###
+extract_results_pr_crude <- function( model_base, identities, exposure ) {
+  results_list <- list()
+  
+  for ( identity in identities ) {
+    
+    model_name <- paste0( "fml_", identity, "_", exposure, "_crude" )
+    model <- model_base[[ model_name ]]
+    
+    if ( !is.null( model ) ) {
+      exp_model <- exp( model )
+      temp_df <- as.data.frame( exp_model )
+      
+      colnames( temp_df ) <- c( paste0( identity, "_point_estimate_crude" ),
+                                paste0( identity, "_lower_ci_crude" ),
+                                paste0( identity, "_upper_ci_crude" ) )
+      
+      results_list[[ identity ]] <- temp_df
+    }
+  }
+  
+  names( results_list ) <- NULL
+  
+  combined_df <- tibble::rownames_to_column(
+    do.call( cbind, results_list ),
+    var = "Exposure" )
+  
+  return( combined_df )
+}
+
+
+### adjusted analysis ###
 extract_results_pr <- function( model_base, model_type, identities, exposure ) {
   results_list <- list()
   
@@ -73,9 +139,9 @@ extract_results_pr <- function( model_base, model_type, identities, exposure ) {
       exp_model <- exp( model )
       temp_df <- as.data.frame( exp_model )
       
-      colnames( temp_df ) <- c( paste0( "Point_estimate_", identity, "_2021" ),
-                                paste0( "Lower_ci_", identity, "_2021" ),
-                                paste0( "Upper_ci_", identity, "_2021" ) )
+      colnames( temp_df ) <- c( paste0( identity, "_point_estimate" ),
+                                paste0( identity, "_lower_ci" ),
+                                paste0( identity, "_upper_ci" ) )
       
       results_list[[ identity ]] <- temp_df
     }
