dbcbbd0c9093506312248175ef3f4fc4325808ce
Willi <guoqiang.w.zhang@gmail.com>
2024-03-19 11:39:18 +0100

    finalized multiple imputation for SPHC 2010 and completed survey analyses of imputed datasets in estimation of proporiton of sexual identity across various groups

--- Helper_functions.R
+++ Helper_functions.R
@@ -10,14 +10,14 @@
 fit_model_crude <- function( formula, design ) {
   
   # Poisson regression
- tryCatch(
+  tryCatch(
     svyglm( formula, design = design, family = quasipoisson( link = "log" ) ),
     error = function( e ) {
       message( e$message )
       return ( NULL )
-    }
+      }
   )
-}
+  }
 
 
 ### adjusted analysis ###
@@ -219,7 +219,7 @@ extract_rr_fluidity <- function( model_results ) {
 }
 
 
-##### Function for Estimation of Proportion of Changes in Sexual Identity #####
+##### Function for Estimation of Proportion of Change in Sexual Identity #####
 
 calculate_proportions <- function( data, variable, design ) {
   prop_var <- svyby( 
@@ -251,49 +251,18 @@ calculate_proportions <- function( data, variable, design ) {
 
 ##### Function to Calculate Proportions of Sexual Identities after Imputation #####
 
-### SPHC-B 2021 ###
 # among demographic subgroups
-calc_prop_imp_subgroup_2021 <- function( design, sexual_identities, demog_var ) {
+calc_prop_imp_subgroup <- function( implist, design, sexual_identities, demog_var, year ) {
   
   # calculate sample size
-  sample_size <- lapply( implist_2021_prop_transformed, function( df ) {
-    table( df[[ demog_var]] ) 
-    } )
-  
-  combined_freqs <- as.data.frame( Reduce( "+", sample_size ) / length( implist_2021_prop_transformed ) )
-  colnames( combined_freqs ) <- c( "subgroup", "sample_size_2021" )
-  combined_freqs$sample_size_2021 <- prettyNum( 
-    round( combined_freqs$sample_size_2021, 0 ), big.mark = ",", preserve.width = "none" )
-  
-  # calculate proportions
-  combined_results_list <- list()
-  
-  for ( identity in sexual_identities ) {
-  combined_summary <- summary( MIcombine( 
-    with( design,
-          svyby( formula = as.formula( paste0( "~ I( sexual_identity_2021 == '", identity, "')" ) ),
-                 by = as.formula( paste0( "~", demog_var ) ),
-                 FUN = svyciprop,
-                 method = "beta" ) ) ) )
-  
-  results_df <- rownames_to_column( combined_summary[ , c( "results", "(lower", "upper)" ) ], var = "subgroup" )
-  colnames( results_df ) <- c( "subgroup", paste0( identity, "_point_estimate_2021" ), paste0( identity, "_lower_ci_2021" ), paste0( identity, "_upper_ci_2021" ) )
-  
-  combined_results_list[[ identity ]] <- results_df
-  }
-
-  final_combined_df <- Reduce( function( x, y ) {
-    merge( x, y, by = "subgroup" ) 
-    }, 
-    combined_results_list )
-  
-  final_combined_df <- merge( final_combined_df, combined_freqs, by = "subgroup" )
-  
-  return( final_combined_df )
-}
-
-# in Stockholm County
-calc_prop_imp_overall_2021 <- function( design, sexual_identities ) {
+  sample_size <- lapply( implist, function( df ) {
+    table( df[[ demog_var ]] ) 
+  } )
+  combined_freqs <- as.data.frame( Reduce( "+", sample_size ) / length( implist ) )
+  colnames( combined_freqs ) <- c( "subgroup", "sample_size" )
+  combined_freqs$sample_size <- prettyNum( 
+    round( combined_freqs$sample_size, 0 ), big.mark = ",", preserve.width = "none" )
+  names( combined_freqs )[ names( combined_freqs ) == "sample_size" ] <- paste0( "sample_size_", year )
   
   # calculate proportions
   combined_results_list <- list()
@@ -301,12 +270,42 @@ calc_prop_imp_overall_2021 <- function( design, sexual_identities ) {
   for ( identity in sexual_identities ) {
     combined_summary <- summary( MIcombine( 
       with( design,
-            svyciprop( formula = as.formula( paste0( "~ I( sexual_identity_2021 == '", identity, "')" ) ),
+            svyby( formula = as.formula( paste0( "~ I( sexual_identity_", year, " == '", identity, "')" ) ),
+                   by = as.formula( paste0( "~", demog_var ) ),
+                   FUN = svyciprop,
+                   method = "beta" ) ) ) )
+    
+    results_df <- rownames_to_column( combined_summary[ , c( "results", "(lower", "upper)" ) ], var = "subgroup" )
+    colnames( results_df ) <- c( "subgroup", paste0( identity, "_point_estimate_", year ), paste0( identity, "_lower_ci_", year ), paste0( identity, "_upper_ci_", year ) )
+    
+    combined_results_list[[ identity ]] <- results_df
+  }
+  
+  final_combined_df <- Reduce( function( x, y ) {
+    merge( x, y, by = "subgroup" ) 
+  }, 
+  combined_results_list )
+  
+  final_combined_df <- merge( final_combined_df, combined_freqs, by = "subgroup" )
+  
+  return( final_combined_df )
+}
+
+# in Stockholm County
+calc_prop_imp_overall <- function( design, sexual_identities, year ) {
+  
+  # calculate proportions
+  combined_results_list <- list()
+  
+  for ( identity in sexual_identities ) {
+    combined_summary <- summary( MIcombine( 
+      with( design,
+            svyciprop( formula = as.formula( paste0( "~ I( sexual_identity_", year, " == '", identity, "')" ) ),
                        FUN = svyciprop,
                        method = "beta" ) ) ) )
     
     results_df <- rownames_to_column( combined_summary[ , c( "results", "(lower", "upper)" ) ], var = "subgroup" )
-    colnames( results_df ) <- c( "subgroup", paste0( identity, "_point_estimate_2021" ), paste0( identity, "_lower_ci_2021" ), paste0( identity, "_upper_ci_2021" ) )
+    colnames( results_df ) <- c( "subgroup", paste0( identity, "_point_estimate_", year ), paste0( identity, "_lower_ci_", year ), paste0( identity, "_upper_ci_", year ) )
     results_df[ 1, 1 ] <- "Stockholm County"
     
     combined_results_list[[ identity ]] <- results_df
@@ -317,8 +316,28 @@ calc_prop_imp_overall_2021 <- function( design, sexual_identities ) {
   }, 
   combined_results_list )
   
-  final_combined_df$sample_size_2021 <- prettyNum( 
-    nrow( d_2021 ), big.mark = ",", preserve.width = "none" )
+  final_combined_df$sample_size <- prettyNum( 
+    nrow( get( paste0( "d_", year ) ) ), big.mark = ",", preserve.width = "none" )
+  names( final_combined_df )[ names( final_combined_df ) == "sample_size" ] <- paste0( "sample_size_", year )
   
   return( final_combined_df )
 }
+
+
+##### Function for Weighted Poisson Regression Model after Imputation #####
+
+fit_model_imp <- function( formula, design ) {
+  
+ # Poisson regression
+ model_result <- summary( MIcombine(
+   with( design,
+         svyglm( formula, family = quasipoisson( link = "log" ) ) )
+   ) )
+ 
+ model_result_selected <- rownames_to_column( model_result[ , c( "results", "(lower", "upper)" ) ], var = "subgroup" )
+ 
+ model_result_selected[ , -1 ] <- exp( model_result_selected[ , -1 ] )
+ 
+ return( model_result_selected )
+}
+
